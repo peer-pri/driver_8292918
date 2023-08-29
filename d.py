063b494f5831affa4edfbe473819ff5942f4b04c
peer-pri <109176503+peer-pri@users.noreply.github.com>
2023-08-29 09:23:29 +0200

    Update d.py

--- d.py
+++ d.py
@@ -1,30 +1,76 @@
+import json
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+import random
 import time
-from tkinter import *
-from tkinter.ttk import *
 
-window = Tk()
+months = ["Januar", "Februar", "März", "April", "Mai", "Juni",
+          "Juli", "August", "September", "Oktober", "November", "Dezember"]
 
-window.title("message spammer")
+def main(bot, username, password, timeout):
+    try:
+        day = Select(bot.find_element_by_xpath('//*[@id="DayDropdown"]'))
+    except:
+        main(bot, username, password, timeout)
+    month = Select(bot.find_element_by_xpath('//*[@id="MonthDropdown"]'))
+    year = Select(bot.find_element_by_xpath('//*[@id="YearDropdown"]'))
+    usernameInput = bot.find_element_by_xpath('//*[@id="signup-username"]')
+    passwordInput = bot.find_element_by_xpath('//*[@id="signup-password"]')
+    register = bot.find_element_by_xpath('//*[@id="signup-button"]')
 
+    # Dob - Day Selector
+    dayVal = random.randint(1, 27)
+    if dayVal < 10:
+        dayVal = "0" + str(dayVal)
+    day.select_by_value(str(dayVal))
 
-def clicked():
-    message = "grc"
-    keyboard = Controller()
+    # Dob - Month Selector
+    monthVal = random.choice(months)
+    month.select_by_visible_text(str(monthVal))
 
-    time.sleep(5)
+    # Dob - Year Selector
+    yearVal = random.randint(1960, 2010)
+    year.select_by_visible_text(str(yearVal))
 
-    for num in range(100):
-        for letter in message:
-            keyboard.press(letter)
-            keyboard.release(letter)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(1)
+    # Username
+    usernameInput.clear()
+    usernameInput.send_keys(username)
 
+    # Password
+    passwordInput.clear()
+    passwordInput.send_keys(password)
 
-btn = Button(window, text="Start", command=clicked)
+    # Register
+    register.click()
+    time.sleep(timeout)
 
-btn.grid(column=0, row=0)
+def login(username: str, password: str, timeout: int, proxyList, headless: bool = True) -> None:
+    options = Options()
+    options.headless = headless
 
-window.geometry("350x350")
-window.mainloop()
+    if proxyList != None:
+        proxy = random.choice(proxyList)
+        options.add_argument(f'--proxy-server={proxy}')
+        time.sleep(12)
+
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    bot = webdriver.Chrome(chrome_options=options)
+    bot.get("https://roblox.com")
+
+    try:
+        bot.find_element_by_xpath('//*[@id="cookie-banner-wrapper"]/div[1]/div[2]/div/div/button[2]')
+    except:
+        try:
+            bot.find_element_by_xpath('//*[@id="DayDropdown"]')
+        except:
+            login(username, password, timeout, proxyList, headless)
+        login(username, password, timeout, proxyList, headless)
+
+    if len(username) > 20:
+        username = str(username[:20])
+
+    try:
+        main(bot, username, password, timeout)
+    except:
+        bot.close()
